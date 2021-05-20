3af7ab2594f55cf81bec84ab52efa3dc2de013ed
Nirvan-sharma <78906217+Nirvan-sharma@users.noreply.github.com>
2021-05-20 15:58:44 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+var player1=document.getElementById("player1_name_input").value;
+var player1=document.getElementById("player2_name_input").value;
+
+function nextscreen(){
+    window.location="play.html";
+}
+
